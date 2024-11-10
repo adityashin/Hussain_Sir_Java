9d5a753c1701990e11a053f8b9c2678b06a38bc9
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-11-10 23:28:49 +0530

    Add files via upload

---
+++ IACSD Core JAVA/Day2/Day3.3/PrimitiveArrays.java/Test1.java
@@ -0,0 +1,15 @@
+package PrimitiveArrays;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter size of array");
+        double[] array;
+
+        array = new double[sc.nextInt()];
+        sc.close();
+    }
+}
